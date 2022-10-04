65b6e53eac73a19672ba5128771a53d9acc5f7c7
Humberto <hldtux@gmail.com>
2022-10-04 22:54:10 +0200

    Adding idCurso

--- db.sh
+++ db.sh
@@ -12,13 +12,12 @@ sqlite-utils insert $DB curso curso.json --pk=id --replace
 CURSO_COUNT=$(cat curso.json | wc -l)
 for CURSO_ID in $(cat curso.json | jq -r .[].id | sort -n); do
     echo "Downloading: $CURSO_ID/$CURSO_COUNT"
-    curl -s http://sistemasparainternet.azurewebsites.net/horarios/2.0/getHorarios.php?id=$CURSO_ID | jq .horarios | sqlite-utils insert $DB horario - --pk=id --replace &
-    curl -s http://sistemasparainternet.azurewebsites.net/horarios/2.0/getMensagens.php?id=$CURSO_ID | jq .mensagens | sqlite-utils insert $DB mensagem - --pk=id --replace &
+    curl -s http://sistemasparainternet.azurewebsites.net/horarios/2.0/getHorarios.php?id=$CURSO_ID | jq '.horarios' | jq --arg val $CURSO_ID '.[] += {idCurso: $val}' | sqlite-utils insert $DB horario - --pk=id --replace &
+    curl -s http://sistemasparainternet.azurewebsites.net/horarios/2.0/getMensagens.php?id=$CURSO_ID | jq '.mensagens' | jq --arg val $CURSO_ID '.[] += {idCurso: $val}' | sqlite-utils insert $DB mensagem - --pk=id --replace &
 done
 rm curso.json
 
 curl -s http://sistemasparainternet.azurewebsites.net/horarios/2.0/getSalas.php | jq .salas | sqlite-utils insert $DB sala - --pk=id --replace
-
 
 echo "Waiting for processes to finish" 
 wait $(jobs -p)
